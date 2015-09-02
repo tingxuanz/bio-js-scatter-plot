36736dd8961be2de03bfb586244df891ebba1d00
Ariane Mora <ariane.mora@uq.net.au>
2015-09-02 10:13:13 +1000

    options.graph has been updated. Includes dt and median options.

--- examples/simple.js
+++ examples/simple.js
@@ -29,7 +29,9 @@ var tooltip = d3.tip()
         return temp; 
     });
 
-data_url= '../data/ds_id_2000_scatter_stat1.tsv';//'../data/ds_id_2000_scatter_pdgfd.tsv';
+//data_url= '../data/ds_id_5003_scatter_gata3.tsv';
+data_url = '../data/ds_id_2000_scatter_stat1.tsv';
+//data_url = '../data/ds_id_2000_scatter_pdgfd.tsv';
 d3.tsv(data_url,function (error,data){
     
     count = 0; 
@@ -57,10 +59,11 @@ d3.tsv(data_url,function (error,data){
         background_colour: "white",
         background_stroke_colour:  "black",
         background_stroke_width:  "1px",
-        circle_radius:4.5,  // for the scatter points
+        circle_radius:3.5,  // for the scatter points
         data: data,
         data_columns_for_colour: ["MSC_calls","ds_id"], //d.MSC_calls
         domain_colours : ["#FFFFFF","#7f3f98"],
+        dt: 5,
         error_bar_width:10, 
         height: 1500,
         horizontal_line_value_column: 'value',
@@ -68,6 +71,7 @@ d3.tsv(data_url,function (error,data){
         legend_class: "legend",
         legend_range: [0,100],
         margin:{top: 180, right: 120, bottom: 530, left: 200},
+        med: 8.33,
         sample_type_order: "DermalFibroblast, hONS", // "BM MSC,BM erythropoietic cells CD235A+,BM granulopoietic cells CD11B+,BM hematopoietic cells CD45+,Developing cortex neural progenitor cells,Ventral midbrain neural progenitor cells,Olfactory lamina propria derived stem cells",
         show_horizontal_line_labels: true,
         subtitle1: subtitle1,
