5cb9af45132ab429c3e0df6e7c6480c3366504c3
Ariane Mora <ariane.mora@uq.net.au>
2015-09-07 13:09:43 +1000

    Legend now supports long names by increasing the size of the canvas

--- examples/simple.js
+++ examples/simple.js
@@ -29,20 +29,46 @@ var tooltip = d3.tip()
         return temp; 
     });
 
-data_url= '../data/ds_id_5003_scatter_gata3.tsv';
+//data_url= '../data/ds_id_5003_scatter_gata3.tsv';
 //data_url = '../data/ds_id_2000_scatter_stat1.tsv';
-//data_url = '../data/ds_id_2000_scatter_pdgfd.tsv';
+data_url = '../data/ds_id_2000_scatter_pdgfd.tsv';
 d3.tsv(data_url,function (error,data){
-    
+    max = 0; 
+    min = 0;
+    probe_name_length = 0;
+    number_of_increments = 0;
     count = 0; 
     data.forEach(function(d){
         // ths + on the front converts it into a number just in case
         d.Expression_Value = +d.Expression_Value;
         d.Standard_Deviation = +d.Standard_Deviation;
+        d.Probe = d.Probe;
+        //calculates the max value of the graph otherwise sets it to 0
+        //calculates the min value and uses this if max < 0 otherwise sets to 0
+        //increment valye = max - min.
+        if(d.Expression_Value + d.Standard_Deviation > max){
+            max = d.Expression_Value + d.Standard_Deviation;
+        }
+        if(d.Expression_Value - d.Standard_Deviation < min){
+            min = d.Expression_Value - d.Standard_Deviation;
+        }
+        if((d.Probe).length > probe_name_length){
+            probe_name_length = (d.Probe).length;
+        }
         count++;
 
     });
-
+    number_of_increments = max - min;
+    //turn number of increments into a whole number
+    number_of_increments |= 0;
+    //this deals with very long probe names. If they are too large
+    //increase the size of the graph to compensate 
+    if(probe_name_length > 22){
+        probe_name_length = 15* probe_name_length;
+    }else{
+        probe_name_length = 450;
+    }
+    probe_name_length = probe_name_length;
     title = "Scatter Plot";
     subtitle1 = "Subtitle"
     subtitle2 = "Subtitle"
@@ -50,7 +76,7 @@ d3.tsv(data_url,function (error,data){
 
     // can always use just a straight value, but it's nicer when you calculate
     // based off the number of samples that you have
-    width = data.length*5 + 200;
+    width = data.length*3 + 500;
     if (width < 1000){
         width = 1000;
     }
@@ -64,18 +90,26 @@ d3.tsv(data_url,function (error,data){
         data_columns_for_colour: ["MSC_calls","ds_id"], //d.MSC_calls
         domain_colours : ["#FFFFFF","#7f3f98"],
         dt: 5,
-        error_bar_width:10, 
+        dt_colour: "green",
+        error_bar_width:5,
+        error_dividor:100,//100 means error bars will not show when error < 1% value 
         height: 1500,
+    //    horizontal_lines: horizontal_lines,
         horizontal_line_value_column: 'value',
-//        horizontal_lines: horizontal_lines,  // this gets turned into an array of objects
         legend_class: "legend",
+        increment: number_of_increments,
         legend_range: [0,100],
-        margin:{top: 180, right: 120, bottom: 530, left: 200},
+        line_stroke_width: "2px",
+        margin_legend: width - 190,
+        margin:{top: 180, left:200, bottom: 530, right: probe_name_length},
         med: 8.93,
+        med_colour: "purple",
+        probe_length: probe_name_length,
         sample_type_order: "DermalFibroblast, hONS", // "BM MSC,BM erythropoietic cells CD235A+,BM granulopoietic cells CD11B+,BM hematopoietic cells CD45+,Developing cortex neural progenitor cells,Ventral midbrain neural progenitor cells,Olfactory lamina propria derived stem cells",
         show_horizontal_line_labels: true,
         subtitle1: subtitle1,
         subtitle2: subtitle2,
+        stroke_width:"1.5px",
         target: target,
         title: title,
         title_class: "title",
