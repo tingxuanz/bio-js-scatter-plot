9f625dc150da7f617f3712358bb650b65f2201ce
tingxuanz <zhangtx93@gmail.com>
2016-12-13 13:33:30 +1100

    update wrap function in general.js

--- examples/1_sample_small.js
+++ examples/1_sample_small.js
@@ -5,15 +5,7 @@ function round_to_two_decimal_places(num){
     return new_num;
 }
 
-//An array of colours which are used for the different probes
-/*
-var colours = ["DarkOrchid", "Orange", "DodgerBlue", "Blue","BlueViolet","Brown", "Deeppink", "BurlyWood","CadetBlue",
-"Chartreuse","Chocolate","Coral","CornflowerBlue","Crimson","Cyan", "Red", "DarkBlue",
-"DarkGoldenRod","DarkGray", "Tomato", "Violet","DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen",
-"DarkOrange","DarkOrchid","DarkRed","DarkSalmon","DarkSlateBlue","DarkTurquoise",
-"DarkViolet","DeepPink","DeepSkyBlue","DodgerBlue","FireBrick","ForestGreen","Fuchsia",
-"Gold","GoldenRod","Green","GreenYellow","HotPink","IndianRed","Indigo"];
-*/
+
 var dataset_data = {"detectionThreshold": "6.2", "probeColours": ["red", "Orange", "DodgerBlue", "Blue", "BlueViolet", "Brown", "Deeppink", "BurlyWood", "CadetBlue", "Chartreuse", "Chocolate", "Coral", "CornflowerBlue", "Crimson", "Cyan", "Red", "DarkBlue", "DarkGoldenRod", "DarkGray", "Tomato", "Violet", "DarkGreen", "DarkKhaki", "DarkMagenta", "DarkOliveGreen", "DarkOrange", "DarkOrchid", "DarkRed", "DarkSalmon", "DarkSlateBlue", "DarkTurquoise", "DarkViolet", "DeepPink", "DeepSkyBlue", "DodgerBlue", "FireBrick", "ForestGreen", "Fuchsia", "Gold", "GoldenRod", "Green", "GreenYellow", "HotPink", "IndianRed", "Indigo"], "sampleTypeDisplayGroupColours": {"0": {"0": "#9932cc", "1": "#7a28a3", "2": "#5b1e7a", "3": "#3c1451", "4": "#1d0a28", "5": "#000000"}, "1": {"0": "#ffa500", "1": "#d48900", "2": "#a96d00", "3": "#7e5100", "4": "#533500", "5": "#281900", "6": "#000000"}, "2": {"0": "#1e90ff", "1": "#1460aa", "2": "#0a3055", "3": "#000000"}, "3": {"0": "#0000ff", "1": "#0000e2", "2": "#0000c5", "3": "#0000a8", "4": "#00008b", "5": "#00006e", "6": "#000051", "7": "#000034", "8": "#000017", "9": "#000000"}, "4": {"0": "#8a2be2", "1": "#5c1c96", "2": "#2e0d4a", "3": "#000000"}, "5": {"0": "#a52a2a", "1": "#8d2424", "2": "#751e1e", "3": "#5d1818", "4": "#451212", "5": "#2d0c0c", "6": "#150606", "7": "#000000"}, "6": {"0": "#8b008b", "1": "#6f006f", "2": "#530053", "3": "#370037", "4": "#1b001b", "5": "#000000"}, "7": {"0": "#ff1493", "1": "#df1180", "2": "#bf0e6d", "3": "#9f0b5a", "4": "#7f0847", "5": "#5f0534", "6": "#3f0221", "7": "#1f-10e", "8": "#000000"}}, "Title": "Densely interconnected transcriptional circuits control cell states in human hematopoiesis", "y_axis_label": "Log2 Expression", "medianDatasetExpression": "8.81", "sampleTypeDisplayOrder": "Hematopoietic Stem Cell CD133+ CD34(dim),Hematopoietic Stem Cell CD38- CD34+,Common Myeloid Progenitor,Megakaryocyte/Erythroid Progenitor,Erythroid CD34+ CD71+ GlyA-,Erythroid CD34- CD71+ GlyA-,Erythroid CD34- CD71+ GlyA+,Erythroid CD34- CD71(lo) GlyA+,Erythroid CD34- CD71- GlyA+,Colony Forming Unit-Megakaryocytic (CFU-MK),Megakaryocyte,Granulocyte-Monocyte Progenitor,Colony Forming Unit-Granulocyte (CFU-G),Neutrophilic Metamyelocyte,Neutrophil,Colony Forming Unit-Monocyte (CFU-M),Monocyte,Eosinophil,Basophil,Myeloid Dendritic cell,Plasmacytoid Dendritic cell,Early B-cell,Pro B-cell,Naive B-cell,Mature B-cell (class able to switch),Mature B-cell,Mature B-cell (class switched),Mature NK Cell CD56- CD16+ CD3-,Mature NK Cell CD56+ CD16+ CD3-,Mature NK Cell CD56- CD16- CD3-,NKT,Naive T-cell CD8+,Effective Memory RA T-cell CD8+,Effective Memory T-cell CD8+,Central Memory T-cell CD8+,Naive T-cell CD4+,Effective Memory T-cell CD4+,Central Memory T-cell CD4+", "limitSortBy": "Sample Type", "cellsSamplesAssayed": "38 distinct hematopoietic cell states based on cell surface marker expression, representing hematopoietic stem and progenitor cells, terminally differentiated cells, and intermediate states", "sampleTypeDisplayGroups": "{\"Hematopoietic Stem Cell CD133+ CD34(dim)\":0,\"Hematopoietic Stem Cell CD38- CD34+\":0,\"Common Myeloid Progenitor\":0,\"Megakaryocyte/Erythroid Progenitor\":0,\"Erythroid CD34+ CD71+ GlyA-\":1,\"Erythroid CD34- CD71+ GlyA-\":1,\"Erythroid CD34- CD71+ GlyA+\":1,\"Erythroid CD34- CD71(lo) GlyA+\":1,\"Erythroid CD34- CD71- GlyA+\":1,\"Colony Forming Unit-Megakaryocytic (CFU-MK)\":2,\"Megakaryocyte\":2,\"Granulocyte-Monocyte Progenitor\":3,\"Colony Forming Unit-Granulocyte (CFU-G)\":3,\"Neutrophilic Metamyelocyte\":3,\"Neutrophil\":3,\"Colony Forming Unit-Monocyte (CFU-M)\":3,\"Monocyte\":3,\"Eosinophil\":3,\"Basophil\":3,\"Myeloid Dendritic cell\":4,\"Plasmacytoid Dendritic cell\":4,\"Early B-cell\":5,\"Pro B-cell\":5,\"Naive B-cell\":5,\"Mature B-cell (class able to switch)\":5,\"Mature B-cell\":5,\"Mature B-cell (class switched)\":5,\"Mature NK Cell CD56- CD16+ CD3-\":6,\"Mature NK Cell CD56+ CD16+ CD3-\":6,\"Mature NK Cell CD56- CD16- CD3-\":6,\"NKT\":6,\"Naive T-cell CD8+\":7,\"Effective Memory RA T-cell CD8+\":7,\"Effective Memory T-cell CD8+\":7,\"Central Memory T-cell CD8+\":7,\"Naive T-cell CD4+\":7,\"Effective Memory T-cell CD4+\":7,\"Central Memory T-cell CD4+\":7}"};
 var colours = dataset_data["probeColours"];
 
@@ -186,7 +178,7 @@ d3.tsv(data_url,function (error,data){
       ref_name[symbol] = symbol;
     }
 
-    var sortByOption = "Smaple_Type";
+    var sortByOption = "Sample_Type";
 
     var split_sort_by_option = sortByOption.split(',');
     if (split_sort_by_option[0] == "Sample Type") {
@@ -209,7 +201,8 @@ d3.tsv(data_url,function (error,data){
         multi_group: multi_group, // Indicates there is only 1 group on the x axis (probes)
         legend_list: {name: "probes", list: probes_types},
         colour_array: probe_colour, //probe_colours=
-        /******** Options for Sizing *****************************************/
+
+        //Options for Sizing
         legend_padding: 190,
         legend_rect_size: 20,
         height: 400,
@@ -230,7 +223,7 @@ d3.tsv(data_url,function (error,data){
 
         circle_radius: 2,  // for the scatter points
         hover_circle_radius: 10,
-        /*********** End of sizing options **********************************/
+        //End of sizing options
 
         background_colour: "white",
         background_stroke_colour:  "black",
@@ -289,6 +282,7 @@ d3.tsv(data_url,function (error,data){
         y_axis_title: dataset_data["y_axis_label"],
         y_column: 'Expression_Value'
     }
+
 
     var instance = new app(options);
 
